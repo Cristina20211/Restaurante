a3afa5b34878e51bfd82d154cda505979497015e
Cristina20211 <87824517+Cristina20211@users.noreply.github.com>
2022-12-12 22:27:47 -0600

    qr

--- app/src/main/java/com/example/restaurante/ActivityQr.kt
+++ app/src/main/java/com/example/restaurante/ActivityQr.kt
@@ -3,7 +3,7 @@ package com.example.restaurante
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class AyudaInterfaz : AppCompatActivity() {
+class ActivityQr: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_qr)
